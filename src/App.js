40a950efdab0d30010bde4124b8a6fabec394583
Jefferson Santos <jeff-ds@outlook.com>
2023-03-01 15:39:06 -0300

    requisito 2 - feito

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 // Iniciando projeto
 class App extends React.Component {
@@ -7,6 +8,7 @@ class App extends React.Component {
     return (
       <div>
         <Header />
+        <SolarSystem />
       </div>
     );
   }
